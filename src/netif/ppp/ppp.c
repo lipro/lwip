lipro/lwip

commit 987f6237c4d1c5866fea840a5ea46ae4d59f1ecd

PPP, MPPE, drop input/output packets if we couldn't find the chosen decompressor/compressor
Drop input/output packets if we couldn't find a decompressor/compressor,
it can't really happen because we only negotiate what we are able to
compress/decompress, but for the sake of code consistency it makes much
more sense to do so.

src/netif/ppp/ppp.c
@@ -530,7 +530,7 @@ static err_t ppp_netif_output(struct netif *netif, struct pbuf *pb, u16_t protoc
       return err;
 #endif /* MPPE_SUPPORT */
     default:
-      break;
+      goto err_rte_drop; /* Cannot really happen, we only negotiate what we are able to do */
     }
   }
 #endif /* CCP_SUPPORT */
@@ -790,7 +790,7 @@ void ppp_input(ppp_pcb *pcb, struct pbuf *pb) {
       break;
 #endif /* MPPE_SUPPORT */
     default:
-      break;
+      goto drop; /* Cannot really happen, we only negotiate what we are able to do */
     }
 
     /* Assume no PFC */

commit d485307976d8b4d78630500735891e0dd3eade1f

ppp: fix broken ppp_netif_output
Introduced with commit 987f6237c4d1c5866fea840a5ea46ae4d59f1ecd
the ppp output function has no more processed raw packages.

Previously, all PCB with an unknown ccp_transmit_method were
processed w/o any compression methode. With the commit "PPP, MPPE,
drop input/output packets if we couldn't find the chosen
decompressor/compressor" as far as all packages will be droped.

In the case of PPP links w/o MPPE we would lost all PPP output.

Signed-off-by: Stephan Linz <linz@li-pro.net>

src/netif/ppp/ppp.c
@@ -471,6 +471,7 @@ static err_t ppp_netif_output(struct netif *netif, struct pbuf *pb, u16_t protoc
     goto err_rte_drop;
   }
 
+#if CCP_SUPPORT
 #if MPPE_SUPPORT
   /* If MPPE is required, refuse any IP packet until we are able to crypt them. */
   if (pcb->settings.require_mppe &&
@@ -479,6 +480,7 @@ static err_t ppp_netif_output(struct netif *netif, struct pbuf *pb, u16_t protoc
     goto err_rte_drop;
   }
 #endif /* MPPE_SUPPORT */
+#endif /* CCP_SUPPORT */
 
 #if VJ_SUPPORT && LWIP_TCP
   /*
@@ -508,7 +510,7 @@ static err_t ppp_netif_output(struct netif *netif, struct pbuf *pb, u16_t protoc
 #endif /* VJ_SUPPORT && LWIP_TCP */
 
 #if CCP_SUPPORT
-  if (pcb->ccp_is_up) {
+  if (pcb->ccp_is_up && pcb->ccp_transmit_method != 0) {
 #if MPPE_SUPPORT
     err_t err;
 #endif /* MPPE_SUPPORT */
@@ -528,6 +530,7 @@ static err_t ppp_netif_output(struct netif *netif, struct pbuf *pb, u16_t protoc
       return err;
 #endif /* MPPE_SUPPORT */
     default:
+      PPPDEBUG(LOG_ERR, ("ppp_netif_output[%d]: bad TX method\n", pcb->netif->num));
       goto err_rte_drop; /* Cannot really happen, we only negotiate what we are able to do */
     }
   }
@@ -786,6 +789,7 @@ void ppp_input(ppp_pcb *pcb, struct pbuf *pb) {
       break;
 #endif /* MPPE_SUPPORT */
     default:
+      PPPDEBUG(LOG_ERR, ("ppp_input[%d]: bad RX method\n", pcb->netif->num));
       goto drop; /* Cannot really happen, we only negotiate what we are able to do */
     }
 